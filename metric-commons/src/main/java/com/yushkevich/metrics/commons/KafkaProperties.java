986a092aea60c5d4a5ca33f410447c420530804a
Vladimir Yushkevich <yushkevich.vladimir@gmail.com>
2021-02-06 22:06:53 +0100

    Check producer workflow

--- metric-commons/src/main/java/com/yushkevich/metrics/commons/KafkaProperties.java
+++ metric-commons/src/main/java/com/yushkevich/metrics/commons/KafkaProperties.java
@@ -2,7 +2,6 @@ package com.yushkevich.metrics.commons;
 
 public final class KafkaProperties {
 
-    // Trigger workflow
     public static final String TOPIC = "test";
     public static final String KAFKA_SERVER_URL = "localhost";
     public static final int KAFKA_SERVER_PORT = 9092;
